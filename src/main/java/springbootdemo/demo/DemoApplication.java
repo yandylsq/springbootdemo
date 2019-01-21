62a3ab933fdce7a1fd83e8deb208d9b88a485b53
yandylsq <45806460+yandylsq@users.noreply.github.com>
2019-01-21 14:26:57 +0800

    Update DemoApplication.java

--- src/main/java/springbootdemo/demo/DemoApplication.java
+++ src/main/java/springbootdemo/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("测试提交");
+		System.out.println("测试在git上更新");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
